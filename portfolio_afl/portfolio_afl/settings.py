ebaa48ef90323bffde65568ef238cbee2c074a55
Andrea <andreaflongo@gmail.com>
2018-08-27 00:34:22 +0100

    Postgres setup

--- portfolio_afl/portfolio_afl/settings.py
+++ portfolio_afl/portfolio_afl/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'portfolio_afl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_afl_db',
+        'USER': 'postgres',
+        'PASSWORD': 'dyd666!',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
+
     }
 }
 
